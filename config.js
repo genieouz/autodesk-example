6554377dc9664c4a4ad0b7bf66182b86b894a2f6
ousmane <ousmane.sakho@qualshore.com>
2019-09-27 13:14:08 +0000

    add config params

--- config.js
+++ config.js
@@ -6,9 +6,9 @@
       'password': 'FMK9:H2yzJQr(3!R',
       'algorithm': 'aes-256-ctr',
       'credentials': {
-        client_id: "",
-        client_secret: "",
-        callback_url: ""
+        client_id: "4Q6CGwcbmLh7YQ13vSDmxaEYVJ4F1acR",
+        client_secret: "ol1rMQ7DdPguU0I6",
+        callback_url: "http://localhost:3000"
 
       },
       'scopes': {
